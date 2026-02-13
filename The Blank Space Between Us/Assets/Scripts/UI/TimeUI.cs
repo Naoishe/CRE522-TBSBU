9446d347483314eb6975dd932122a088efe94832
Naoishe McAleese <McAleese-N6@ulster.ac.uk>
2026-02-13 15:46:30 +0000

    Sundial UI Testing
    Setting up code for Sundial UI TIme system

--- The Blank Space Between Us/Assets/Scripts/UI/TimeUI.cs
+++ The Blank Space Between Us/Assets/Scripts/UI/TimeUI.cs
@@ -2,21 +2,38 @@ using UnityEngine;
 using System;
 using System.Collections.Generic;
 using System.Collections;
+using System.Timers;
 using TMPro;
 
 public class TimeUI : MonoBehaviour
 {
     
     public TextMeshProUGUI timeText;
+    public Animator sundialAnimator;
+    private float staticTimeFeedingVar;
     //public TimeManager timeManager;
     private int timeIndex;
     private int day;
+    private string timeframe;
+    private Timer staticTimer;
+
+    private void OnEnable()
+    {
+        TimeManager.OnTimeFrameChanged += SundialStatic;
+    }
+    private void OnDisable()
+    {
+        TimeManager.OnTimeFrameChanged -= SundialStatic;
+    }
 
     public void Update()
     {
         timeIndex = TimeManager.TimeFrameIndex;  
         day= TimeManager.Day;
+        timeframe = TimeManager.TimeFrame[timeIndex];
         UpdateTime();
+        UpdateSundial();
+
     }
     
     private void UpdateTime()
@@ -27,4 +44,48 @@ public class TimeUI : MonoBehaviour
         //Debug.Log("Day = " + day);
     }
 
+    private void UpdateSundial()
+    {
+        if (timeframe == "Morning")
+        {
+            sundialAnimator.SetTrigger("Morning");
+        }
+        if (timeframe == "Midday")
+        {
+            sundialAnimator.SetTrigger("Midday");
+        }
+        if (timeframe == "Early Evening")
+        {
+            sundialAnimator.SetTrigger("EarlyEvening");
+        }
+        if (timeframe == "Late Evening")
+        {
+            sundialAnimator.SetTrigger("LateEvening");
+        }
+        if (timeframe == "Night")
+        {
+            sundialAnimator.SetTrigger("Night");
+        }
+    }
+
+    private void SundialStatic()
+    {
+        sundialAnimator.SetTrigger("Static");
+        staticTimer = new Timer(2500); 
+        staticTimer.Start();
+        //staticTimer.Elapsed += StopStatic;
+
+        
+    }
+
+    private void StopStatic(ElapsedEventArgs e)
+    {
+        Console.WriteLine("Time Returned"+e.SignalTime);
+        sundialAnimator.SetTrigger("StaticReset");
+    }
+
+    
+ 
+
 }
+
